6c86da6c825f80571767ba25fcc28ff9cd6be2da
villancikos <villancikos@gmail.com>
2016-10-01 15:35:30 +0100

    Colors added

--- module1solution/app.js
+++ module1solution/app.js
@@ -7,18 +7,42 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
   $scope.dishes = "";
   $scope.message = "";
+  $scope.textColor="red";
+  $scope.borderColor=""//black
+  $scope.emptyArrays=0;
 
   $scope.checkLunch = function () {
     var message = produceMessage($scope.dishes);
+    //$scope.emptyArrays = realCount($scope.dishes);
     $scope.message = message;
   };
 
+  function realCount(string){
+    var array = string.split(",");
+    var pointsToTake = 0;
+    if (string.length ==0){
+      console.log("do nothing");
+    }
+    else{
+      for (var i=0;i<=array.length;i++){
+        if (array[i].length<=1){
+          pointsToTake+=1;
+          console.log("puntos contados"+pointsToTake);
+        }
+      }
+      return pointsToTake;
+    }
+  }
   function produceMessage(string) {
     var splittedString = string.split(",")
     if(string.length == 0){
+      $scope.textColor="red";
+      $scope.borderColor="#ff0000"//red
       return "Please enter data first";
     }
     else{
+      $scope.borderColor="#2cbe0c"//green
+      $scope.textColor="#2cbe0c";
       if(splittedString.length<=3){
         return "Enjoy!";
       }
